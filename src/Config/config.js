cef8c5467d4ea5a57740cbcd823517aac7bbb7e2
hossain24 <haj.ctg@gmail.com>
2018-11-05 19:59:12 +0100

    initial

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyDFz11cF3k-1l9fRH1HIJpA2uwODxh-yWE",
+  authDomain: "hill-track-11d1d.firebaseapp.com",
+  databaseURL: "https://hill-track-11d1d.firebaseio.com",
+  projectId: "hill-track-11d1d",
+  storageBucket: "hill-track-11d1d.appspot.com",
+  messagingSenderId: "1048459968019"
+};
